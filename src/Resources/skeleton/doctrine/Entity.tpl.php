92121807f58c3026aa1eb36d74f12b10e547dca2
Romain Monteil <monteil.romain@gmail.com>
2021-06-12 01:03:48 +0200

    Add support for ORM Attribute in make:entity

--- src/Resources/skeleton/doctrine/Entity.tpl.php
+++ src/Resources/skeleton/doctrine/Entity.tpl.php
@@ -9,28 +9,42 @@ use Doctrine\ORM\Mapping as ORM;
 <?php if ($broadcast): ?>use Symfony\UX\Turbo\Attribute\Broadcast;
 <?php endif ?>
 
-/**
-<?php if ($api_resource && !$use_attributes): ?> * @ApiResource()
+<?php if ($use_attributes): ?>
+#[
+<?php if ($api_resource): ?>    ApiResource,
 <?php endif ?>
-<?php if ($broadcast && !$use_attributes): ?> * @Broadcast()
+<?php if ($broadcast): ?>    Broadcast,
+<?php endif ?>
+    ORM\Entity(repositoryClass: <?= $repository_class_name ?>::class),
+<?php if ($should_escape_table_name): ?>    ORM\Table(name: "`<?= $table_name ?>`"),
+<?php endif ?>
+]
+<?php else: ?>
+/**
+<?php if ($api_resource): ?> * @ApiResource()
+<?php endif ?>
+<?php if ($broadcast): ?> * @Broadcast()
 <?php endif ?>
  * @ORM\Entity(repositoryClass=<?= $repository_class_name ?>::class)
 <?php if ($should_escape_table_name): ?> * @ORM\Table(name="`<?= $table_name ?>`")
 <?php endif ?>
- */
-<?php if ($api_resource && $use_attributes): ?>
-#[ApiResource]
-<?php endif ?>
-<?php if ($broadcast && $use_attributes): ?>
-#[Broadcast]
+*/
 <?php endif ?>
 class <?= $class_name."\n" ?>
 {
+    <?php if ($use_attributes): ?>
+    #[
+        ORM\Id,
+        ORM\GeneratedValue,
+        ORM\Column(type: "integer")
+    ]
+    <?php else: ?>
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
+    <?php endif ?>
     private $id;
 
     public function getId(): ?int
